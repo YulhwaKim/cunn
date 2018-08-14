c8cf4febce4d310e31b21955aad6f7729fa65b56
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-14 16:53:01 +0900

    Update VariationModeling.cu

--- lib/THCUNN/VariationModeling.cu
+++ lib/THCUNN/VariationModeling.cu
@@ -49,11 +49,11 @@ __global__ void cunn_VariationModeling_updateOutput_kernel(
     int value = ScalarConvert<T, int>::to(IN[INidx]);
     int rowIdx = (value + accumN) / 2;
     // STEP2. generate reference point
-    AccumT refpoint = REF[INidx];
+    T refpoint = REF[INidx];
     // AccumT refpoint = rand()/(AccumT)RAND_MAX;
     // STEP3. find the column index of probability table and change the data
     for(int j=0; j<nCol; j++) {
-      AccumT prob = PTABLEs[rowIdx*nCol + j];
+      T prob = PTABLEs[rowIdx*nCol + j];
       if(((prob > 0) && (prob > refpoint)) || (j==nCol-1)) {
         OUT[INidx] = ScalarConvert<int, T>::to(value + 2*(j - transitionWindow));
         break;
