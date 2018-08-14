36b7acbc61116ba7df8eec00c731fbaafaaafbf1
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-14 16:38:17 +0900

    Update VariationModeling.cu

--- lib/THCUNN/generic/VariationModeling.cu
+++ lib/THCUNN/generic/VariationModeling.cu
@@ -1,1 +1,69 @@
+#ifndef THC_GENERIC_FILE
+#define THC_GENERIC_FILE "generic/VariationModeling.cu"
+#else
+
+#include "../common.h"
+
+void THNN_(VariationModeling_updateOutput)(
+          THCState *state,
+          THCTensor *output,
+          THCTensor *input,
+          THCTensor *ptable,
+          int accumN,
+          THCTensor *ref) // ref is for debugging
+{
+  THCUNN_assertSameGPU(state, 4, output, input, ptable, ref);
+  
+  // get parameters
+  int ndims = THCTensor_(nDimension)(state,input);
+  if (ndims == 2) {
+    long zdim = 1;
+    long ydim = THCTensor_(size)(state, input, 0);
+    long xdim = THCTensor_(size)(state, input, 1);
+  }
+  if (ndims == 3) {
+    long zdim = THCTensor_(size)(state, input, 0);
+    long ydim = THCTensor_(size)(state, input, 1);
+    long xdim = THCTensor_(size)(state, input, 2);
+  }
+  long nRow = THCTensor_(size)(state, ptable, 0);
+  long nCol = THCTensor_(size)(state, ptable, 1);
+
+  // resize output and make input continuous
+  THCTensor_(resizeAs)(state, output, input)
+  input = THCTensor_(newContiguous)(state, input);
+  
+   // check if BLOCK_SIZE is properly set
+  int check = BLOCK_SIZE;
+  prinf("BLOCK_SIZE shoulbe be 32 and it is '%d'\n", check);
+  
+  // set dimension of block and grid
+  dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 grid((xdim + threads.x - 1)/threads.x, (ydim + threads.y - 1)/threads.y); 
+
+  cunn_VariationModeling_updateOutput_kernel<real, accreal><<<grid, threads, nRow*nCol*sizeof(real)>>(
+          THCTensor_(data)(state, output),
+          THCTensor_(data)(state, input),
+          xdim,
+          ydim,
+          zdim,
+          THCTensor_(data)(state, ptable),
+          nRow,
+          nCol,
+          accumN,
+          THCTensor_(data)(state, ref));
+  
+  // error checking
+  cudaError errcode = cudaGetLastError();
+  if (errcode != cudaSuccess)
+  {
+    THError(cudaGetErrorString(errcode));
+  }
+
+  // free input
+  THCTensor_(free)(state, input);
+}
+
+
+#endif
 
