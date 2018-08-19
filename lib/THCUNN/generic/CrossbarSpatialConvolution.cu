9e2bf5a0a2a937e3a4a373d1f0ef79dde8c09685
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-19 20:18:53 +0900

    Update CrossbarSpatialConvolution.cu

--- lib/THCUNN/generic/CrossbarSpatialConvolution.cu
+++ lib/THCUNN/generic/CrossbarSpatialConvolution.cu
@@ -2,45 +2,45 @@
 #define THC_GENERIC_FILE "generic/CrossbarSpatialConvolution.cu"
 #else
 
-static inline void THNN_(CrossbarSpatialConvolution_shapecheck)(
-                         THCState *state,
-                         THCTensor *input, THCTensor *weight,
-                         int kH, int kW, int dH, int dW, int padH, int padW){
-  THArgCheck(kW > 0 && kH > 0, 9,
-            "kernel size should be greater than zero, but got kH: %d kW: %d", kH, kW);
-  THArgCheck(dW > 0 && dH > 0, 11,
-            "stride should be greater than zero, but got dH: %d dW: %d", dH, dW);
-  THArgCheck(state, weight->nDimension == 2 || weight->nDimension == 4, 5, weight,
-            "2D or 4D weight tensor expected, but got: %s");
+// static inline void THNN_(CrossbarSpatialConvolution_shapecheck)(
+//                          THCState *state,
+//                          THCTensor *input, THCTensor *weight,
+//                          int kH, int kW, int dH, int dW, int padH, int padW){
+//   THArgCheck(kW > 0 && kH > 0, 9,
+//             "kernel size should be greater than zero, but got kH: %d kW: %d", kH, kW);
+//   THArgCheck(dW > 0 && dH > 0, 11,
+//             "stride should be greater than zero, but got dH: %d dW: %d", dH, dW);
+//   THArgCheck(state, weight->nDimension == 2 || weight->nDimension == 4, 5, weight,
+//             "2D or 4D weight tensor expected, but got: %s");
   
-  int ndim = input->nDimension;
-  int dimf = 0;
-  int dimh = 1;
-  int dimw = 2;
+//   int ndim = input->nDimension;
+//   int dimf = 0;
+//   int dimh = 1;
+//   int dimw = 2;
   
-  if (ndim == 4) {
-    dimf++;
-    dimh++;
-    dimw++;
-  }
+//   if (ndim == 4) {
+//     dimf++;
+//     dimh++;
+//     dimw++;
+//   }
   
-  THCUNN_argcheck(state, ndim == 3 || ndim == 4, 2, input,
-                 "3D or 4D input tensor expected but got: %s");
+//   THCUNN_argcheck(state, ndim == 3 || ndim == 4, 2, input,
+//                  "3D or 4D input tensor expected but got: %s");
   
-  long nInputPlane = weight->size[1] / (kH * kW);
-  long inputHeight = input->size[dimh];
-  long inputWidth = input->size[dimw];
-  long nOutputPlane = weight->size[0];
-  long outputHeight = (inputHeight + 2*padH - kH) / dH + 1;
-  long outputWidth = (inputWidth + 2*padW - kW) / dW + 1;
+//   long nInputPlane = weight->size[1] / (kH * kW);
+//   long inputHeight = input->size[dimh];
+//   long inputWidth = input->size[dimw];
+//   long nOutputPlane = weight->size[0];
+//   long outputHeight = (inputHeight + 2*padH - kH) / dH + 1;
+//   long outputWidth = (inputWidth + 2*padW - kW) / dW + 1;
   
-  if (outputWidth < 1 || outputHeight < 1)
-    THError("Given input size: (%d x %d x %d). "
-            "Calculated output size: (%d x %d x %d). Output size is too small", 
-             nInputPlane, inputHeight, inputWidth, nOutputPlane, outputHeight, outputWidth);
+//   if (outputWidth < 1 || outputHeight < 1)
+//     THError("Given input size: (%d x %d x %d). "
+//             "Calculated output size: (%d x %d x %d). Output size is too small", 
+//              nInputPlane, inputHeight, inputWidth, nOutputPlane, outputHeight, outputWidth);
   
-  THCUNN_check_dim_size(state, input, ndim, dimf, nInputPlane); 
-}
+//   THCUNN_check_dim_size(state, input, ndim, dimf, nInputPlane); 
+// }
 
 void THNN_(CrossbarSpatialConvolution_updateoutput)(
            THCState *state,
@@ -66,8 +66,8 @@ void THNN_(CrossbarSpatialConvolution_updateoutput)(
     freeWeight = 1;
   }
   
-  THNN_(CrossbarSpatialConvolution_shapeCheck)
-    (state, input, weight, kH, kW, dH, dW, padH, padW);
+//   THNN_(CrossbarSpatialConvolution_shapeCheck)
+//     (state, input, weight, kH, kW, dH, dW, padH, padW);
   
   // make input contiguous and 4D
   input = THCTensor_(newContiguous)(state, input);
