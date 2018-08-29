69ea672a666d6105feb3ddb240b66ee8e04659c6
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-29 10:21:18 +0900

    Update CrossbarSpatialConvolutionWvar.cu

--- lib/THCUNN/CrossbarSpatialConvolutionWvar.cu
+++ lib/THCUNN/CrossbarSpatialConvolutionWvar.cu
@@ -42,9 +42,11 @@ __global__ void cunn_CrossbarSpatialConvolutionWvar_updateOutput_frame_kernel(
     
     // compute element-size multiplication
     for(unsigned int j=0; j<BLOCK_SIZE; j++) {
-      // multiplication
+      if (i * BLOCK_SIZE + j > nIn)
+        break;
 //       if((Wrow < nOutputPlane) && (INcol < nOutSpatial))
 //         printf("%.1f ", INs[ty][j]);
+      // multiplication
       T temp = INs[ty][j] * Ws[j][tx];
       // Variation modeling
       temp = (temp > 0)? temp + VarPs[j][tx] : temp + VarMs[j][tx];
@@ -62,8 +64,8 @@ __global__ void cunn_CrossbarSpatialConvolutionWvar_updateOutput_frame_kernel(
           psum = (psum > accumN)? accumN : psum;
           psum = (psum < (-1)*accumN)? (-1)*accumN : psum;
         }
-         if((Wrow < nOutputPlane) && (INcol < nOutSpatial))
-            printf("%.1f / %.1f ", INs[ty][j], ScalarConvert<AccumT, T>::to(psum));
+//          if((Wrow < nOutputPlane) && (INcol < nOutSpatial))
+//             printf("%.1f / %.1f ", INs[ty][j], ScalarConvert<AccumT, T>::to(psum));
         // update output_temp
         output_temp += ScalarConvert<AccumT, T>::to(psum);
         // update or reset states
