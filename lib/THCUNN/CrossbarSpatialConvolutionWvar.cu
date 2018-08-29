1ad43c97f0fc3f9b19d95c2460e26ff89fd59ee3
Yulhwa Kim <35885699+YulhwaKim@users.noreply.github.com>
2018-08-29 16:30:05 +0900

    Update CrossbarSpatialConvolutionWvar.cu

--- lib/THCUNN/CrossbarSpatialConvolutionWvar.cu
+++ lib/THCUNN/CrossbarSpatialConvolutionWvar.cu
@@ -50,7 +50,8 @@ __global__ void cunn_CrossbarSpatialConvolutionWvar_updateOutput_frame_kernel(
       T temp = INs[ty][j] * Ws[j][tx];
       // Variation modeling
       temp = (temp > 0)? temp + VarPs[j][tx] : temp + VarMs[j][tx];
-      printf("temp, before var: %.1f , after var: %.1f", INs[ty][j] * Ws[j][tx], temp);
+      if((Wrow < nOutputPlane) && (INcol < nOutSpatial))
+        printf("temp, before var: %.1f , after var: %.1f\n", INs[ty][j] * Ws[j][tx], temp);
       // Accumulation
       psum += temp;
       accumCount += 1;
